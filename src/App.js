99648488bc033e45196397657170aa9c3c382e88
eliashezron <eliashezron23@gmail.com>
2022-06-03 14:23:10 +0000

    hey

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import { Container, Heading, Flex, Spacer, Avatar, Box, Menu, MenuButton, MenuList, MenuItem, Grid } from "@chakra-ui/react";
-import { ByMoralis, useMoralis } from "react-moralis";
-import { Auth } from './components/Auth';
+import { Container, Heading, Flex, Spacer, Text,  Avatar, Stack,Button, Box, Menu, MenuButton, MenuList, MenuItem, Grid } from "@chakra-ui/react";
+import { useMoralis } from "react-moralis";
 import { Routes, Route, Navigate, Link } from 'react-router-dom';
-import { Home } from './components/Home';
-import { Profile } from './components/Profile';
+import {ErrorBox} from './components/Error'
 
 function App() {
-  const { isAuthenticated, logout, user, isAuthUndefined } = useMoralis();
-
+  const { authenticate, isAuthenticating, isAuthenticated, logout, user, isAuthUndefined, authError } = useMoralis();
+if (isAuthenticated){
+    return (
+      <Container>
+        <div>
+          <Button>Logout</Button>
+        </div>
+      </Container>
+    )
+  }
   return (
-    <Container>
-      <Flex my={3}>
-        <Link to="/"><Heading>Home</Heading></Link>
-        <Spacer />
-        {isAuthenticated && <Menu>
-          <MenuButton as={Grid} ><Avatar name={user.attributes.username} /></MenuButton>
-          <MenuList>
-            <MenuItem minH="32px"><Link to="/profile"><Grid>My profile</Grid></Link></MenuItem>
-            <MenuItem minH="32px" onClick={() => logout()}>Logout</MenuItem>
-          </MenuList>
-        </Menu>}
-      </Flex>
-      <hr />
-
-      <Heading my={3}>Welcome, {user ? user.attributes.username : ' authenticate please..'}</Heading>
-      {isAuthenticated ? <Routes>
-        <Route path="/" exact><Home /></Route>
-        <Route path="/profile" exact><Profile /></Route>
-      </Routes> : <>
-        {!isAuthUndefined && <Navigate replace to="/" />}
-        <Auth />
-      </>}
-      <Box mt={6}><ByMoralis width={150} variant="colour" /></Box>
-    </Container>
+    
+      <Stack spacing={6}>
+        {authError && (<ErrorBox title="Authentication has failed!" message={authError.message} />)}
+        <Button isLoading={isAuthenticating} onClick={() => authenticate()}>Authenticate via Metamask</Button>
+    </Stack>
+    
   );
 }
 
