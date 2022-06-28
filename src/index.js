af12f670110cbae14dd64ecd823fc35a01522ef7
eliashezron <eliashezron23@gmail.com>
2022-06-28 14:02:16 +0000

    commented

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
+// api = EtbPcS47NFrQWvHteJYkLaUPAjdtXeFj
+// relayer = uU122ER5oKDTPF6T2qfmAiXhqFm49UTEzHR25fGZnqD6FXxeiSr1hTxb8MJSsqcE
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
