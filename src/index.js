6df2b7c66fdf83f1526eec2169c5e445f8ac53e4
eliashezron <eliashezron23@gmail.com>
2022-08-13 06:48:49 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { MoralisProvider } from "react-moralis";
+import { Mainnet, DAppProvider, Config, Rinkeby } from "@usedapp/core"
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -12,16 +12,29 @@ const theme = extendTheme({
     initialColorMode: 'dark',
   }
 })
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]:
+      "https://mainnet.infura.io/v3/a22b6958cc5449a6a5bc6dc4e2c26a7a",
+    [Rinkeby.chainId]:
+      "https://rinkeby.infura.io/v3/a22b6958cc5449a6a5bc6dc4e2c26a7a",
+  },
+  notifications: {
+    expirationPeriod: 10000,
+    checkInterval: 1000,
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="Xwn0c822Im69mCfaegmN5ORzwq1diKogh8XCfnMI" serverUrl="https://xjoxkzfjwsba.usemoralis.com:2053/server">
-      <ChakraProvider theme={theme}>
+    <DAppProvider config={config}> 
+    <ChakraProvider theme={theme}>
         <BrowserRouter>
           <App isServerInfo/>
         </BrowserRouter>
       </ChakraProvider>
-    </MoralisProvider>
+      </DAppProvider> 
   </React.StrictMode>,
   document.getElementById('root')
 )
