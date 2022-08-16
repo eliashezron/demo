172160f2595d79817c16c51e0050f54a5ffc642c
eliashezron <eliashezron23@gmail.com>
2022-08-16 10:10:42 +0000

    deploy

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,58 +1,133 @@
-import { Box, Button, Container, Flex, Image, VStack, Text} from "@chakra-ui/react"
-import React from "react"
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Image,
+  VStack,
+  Text,
+  Input,
+  InputLeftAddon,
+  InputRightAddon,
+  InputGroup,
+} from "@chakra-ui/react"
+import React, { useState } from "react"
 import { Contract } from "@ethersproject/contracts"
 import m2 from "../images/m2.png"
 // import m1 from "../images/m1.png"
 import bg from "../images/bg.png"
-import {
-  useEthers,
-  useContractFunction,
-  useNotifications,
-  useEtherBalance,
-} from "@usedapp/core"
-import { utils, BigNumber } from "ethers"
+import { useContractFunction } from "@usedapp/core"
+import { utils } from "ethers"
 
-const Hero=()=>{
-  const nftAddress='0x6f6fc7D4967089b9FC054911bcec13248757d241'
-  const abi=[{"inputs":[{"internalType":"uint256","name":"count","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"}]
+const Hero = () => {
+  const [amount, setAmount] = useState(0.012)
+  const nftAddress = "0x6f6fc7D4967089b9FC054911bcec13248757d241"
+  const abi = [
+    {
+      inputs: [],
+      name: "Mint",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+  ]
   const nftInterface = new utils.Interface(abi)
   const contract = new Contract(nftAddress, nftInterface)
   const { state, send } = useContractFunction(contract, "mint", {
     transactionName: "mintnfttransaction",
   })
- 
-  
-    return(
-        <VStack minheight='85vh'>
-        <Container maxW='md'  centerContent>
-         <Text fontSize='2xl'color='#D7C19A'>Blockchain for everyone</Text> 
-        </Container>
-        <Container maxW='75%' color='white'centerContent pt='0.3%'>
-          <Flex align="center" justify="center"><Text bgGradient='linear(to-l, #7928CA, #FF0080)'
-  bgClip='text'
-  fontSize='6xl'
-  fontWeight='extrabold'>Discovery</Text><Image src={m2} alt='nft' style={{margin:'0 5%'}}/><Text  bgGradient='linear(to-l, #7928CA, #FF0080)'
-  bgClip='text'
-  fontSize='4xl'
-  fontWeight='extrabold'>Collect</Text></Flex>
-        </Container>
-        <Container maxW='75%' color='white'centerContent pt='0.3%'><Text  bgGradient='linear(to-l, #7928CA, #FF0080)'
-  bgClip='text'
-  fontSize='3xl'
-  fontWeight='extrabold'>Remarkable Nfts</Text></Container>
-        <Container centerContent pt='1%'>
-            <Button size='lg' bgGradient={[
-    'linear(to-tr, teal.300, yellow.400)',
-    'linear(to-t, blue.200, teal.500)',
-    'linear(to-b, orange.100, purple.300)',
-  ]} > 
+  const mintNft = (amount) => {
+    send({
+      value: utils.parseEther(amount.toString()),
+    })
+  }
+  const handleMint = (amount) => {
+    mintNft(amount)
+  }
+  return (
+    <VStack minheight='85vh'>
+      <Container maxW='md' centerContent>
+        <Text fontSize='2xl' color='#D7C19A'>
+          Blockchain for everyone
+        </Text>
+      </Container>
+      <Container maxW='75%' color='white' centerContent pt='0.3%'>
+        <Flex align='center' justify='center'>
+          <Text
+            bgGradient='linear(to-l, #7928CA, #FF0080)'
+            bgClip='text'
+            fontSize='6xl'
+            fontWeight='extrabold'
+          >
+            Discovery
+          </Text>
+          <Image src={m2} alt='nft' style={{ margin: "0 5%" }} />
+          <Text
+            bgGradient='linear(to-l, #7928CA, #FF0080)'
+            bgClip='text'
+            fontSize='4xl'
+            fontWeight='extrabold'
+          >
+            Collect
+          </Text>
+        </Flex>
+      </Container>
+      <Container maxW='75%' color='white' centerContent pt='0.3%'>
+        <Text
+          bgGradient='linear(to-l, #7928CA, #FF0080)'
+          bgClip='text'
+          fontSize='3xl'
+          fontWeight='extrabold'
+        >
+          Remarkable Nfts
+        </Text>
+      </Container>
+      <Container centerContent pt='1%'>
+        <Flex>
+          <InputGroup mr='5' size='lg' color='white'>
+            <InputLeftAddon
+              children='+'
+              onClick={() => setAmount(amount + 0.001)}
+            />
+            <Input
+              variant='outline'
+              type='text'
+              name='text'
+              placeholder='enter Amount'
+              value={`${amount.toFixed(3)}`}
+              onChange={(e) => setAmount(e.target.value)}
+              color='white'
+              disabled
+            ></Input>
+            <InputRightAddon
+              children='-'
+              onClick={() => setAmount(amount - 0.001)}
+            />
+          </InputGroup>
+          <Button
+            size='lg'
+            bgGradient={[
+              "linear(to-tr, teal.300, yellow.400)",
+              "linear(to-t, blue.200, teal.500)",
+              "linear(to-b, orange.100, purple.300)",
+            ]}
+            onClick={handleMint}
+            isLoading={state.status === "Mining"}
+          >
             Mint NOW
-            </Button>
-        </Container>
-        <Box border='none' centerContent>
-            <Image src={bg} alt='apes' objectFit='cover'borderRadius='2%'maxheight='700px' />
-        </Box>
-      </VStack>
-    )
+          </Button>
+        </Flex>
+      </Container>
+      <Box border='none' centerContent>
+        <Image
+          src={bg}
+          alt='apes'
+          objectFit='cover'
+          borderRadius='2%'
+          maxheight='700px'
+        />
+      </Box>
+    </VStack>
+  )
 }
 export default Hero
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,73 +1,129 @@
-import { Button, Container, Flex,Box, Text, Modal,
-    ModalOverlay,Divider,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalCloseButton,Image, useDisclosure} from "@chakra-ui/react"
+import {
+  Button,
+  Container,
+  Flex,
+  Box,
+  Text,
+  Modal,
+  ModalOverlay,
+  Divider,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Image,
+  useDisclosure,
+} from "@chakra-ui/react"
 import WalletConnectIcon from "../images/walletconnectlogo.png"
 import metamask from "../images/metamaskWallet.png"
 import React from "react"
-import {
-    useEthers,
-  } from "@usedapp/core"
+import { useEthers } from "@usedapp/core"
 import WalletConnectProvider from "@walletconnect/web3-provider"
 
-const NavBar=()=>{
-    const { activateBrowserWallet, account, activate, deactivate } = useEthers()
-    const isConnected = account !== undefined
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    async function onConnect() {
-        try {
-          const provider = new WalletConnectProvider({
-            infuraId: "a22b6958cc5449a6a5bc6dc4e2c26a7a",
-          })
-          await provider.enable()
-          await activate(provider)
-        } catch (error) {
-          console.error(error)
-        }
-      }
-    return(
-        <>
-        <Container maxW='100%' p='2% 10%' align='center' minh='15vh'>
-            <Flex align='center' justify='space-between'>
-            <Text>BlockBunch</Text>
-            {!isConnected ? 
-            <Button colorScheme='yellow' size='lg'  onClick={onOpen}>
-                Connect Wallet
+const NavBar = () => {
+  const { activateBrowserWallet, account, activate, deactivate } = useEthers()
+  const isConnected = account !== undefined
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  async function onConnect() {
+    try {
+      const provider = new WalletConnectProvider({
+        infuraId: "a22b6958cc5449a6a5bc6dc4e2c26a7a",
+      })
+      await provider.enable()
+      await activate(provider)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return (
+    <>
+      <Container maxW='100%' p='2% 10%' align='center' minh='15vh'>
+        <Flex align='center' justify='space-between'>
+          <Text>BlockBunch</Text>
+          {!isConnected ? (
+            <Button
+              colorScheme='yellow'
+              size='lg'
+              onClick={isConnected ? deactivate : onOpen}
+            >
+              Connect Wallet
             </Button>
-            :<Box><Text color='whiteAlpha.700'>{account}</Text><Button colorScheme='yellow' size='lg' onClick={deactivate}>Disconnect</Button></Box>}
-            </Flex>
-        </Container>
-           <Modal isOpen={isOpen} onClose={onClose}>
-           <ModalOverlay />
-           <ModalContent>
-             <ModalHeader>Connect Your Wallet</ModalHeader>
-             <ModalCloseButton />
-             <ModalBody>
-             <Divider orientation='horizontal' />
-                <Box maxW='sm' p='2' onClick={activateBrowserWallet}  _hover={{
-                        cursor: "pointer",
-                    }}>
-                    <Flex align='center' justify='center'>
-                    <Image src={metamask} alt='metamask' boxSize='50px' mr='5'/>
-                    <Text>MetaMask</Text>
-                    </Flex>
-                </Box>
-                <Divider orientation='horizontal' />
-                <Box maxW='sm' p='2'  onClick={onConnect}   _hover={{
-                        cursor: "pointer",
-                    }}>
-                    <Flex align='center' justify='center'>
-                    <Image src={WalletConnectIcon} alt='metamask' boxSize='50px' mr='5'/>
-                    <Text>Wallet Connect</Text>
-                    </Flex>
-                </Box>
-                <Divider orientation='horizontal' />
-             </ModalBody>
-           </ModalContent>
-         </Modal>
-         </>
-    )
+          ) : (
+            <Box>
+              <Flex>
+                <Text
+                  color='whiteAlpha.700'
+                  align='center'
+                  mr='5'
+                  fontSize='2em'
+                >
+                  {getEllipsisTxt(account, 5)}
+                </Text>
+                <Button colorScheme='yellow' size='lg' onClick={deactivate}>
+                  Disconnect
+                </Button>
+              </Flex>
+            </Box>
+          )}
+        </Flex>
+      </Container>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Connect Your Wallet</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Divider orientation='horizontal' />
+            <Box
+              maxW='sm'
+              p='2'
+              onClick={activateBrowserWallet}
+              _hover={{
+                cursor: "pointer",
+              }}
+            >
+              <Flex align='center' justify='center'>
+                <Image src={metamask} alt='metamask' boxSize='50px' mr='5' />
+                <Text>MetaMask</Text>
+              </Flex>
+            </Box>
+            <Divider orientation='horizontal' />
+            <Box
+              maxW='sm'
+              p='2'
+              onClick={onConnect}
+              _hover={{
+                cursor: "pointer",
+              }}
+            >
+              <Flex align='center' justify='center'>
+                <Image
+                  src={WalletConnectIcon}
+                  alt='metamask'
+                  boxSize='50px'
+                  mr='5'
+                />
+                <Text>Wallet Connect</Text>
+              </Flex>
+            </Box>
+            <Divider orientation='horizontal' />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+/**
+ * Returns a string of form "abc...xyz"
+ * @param {string} str string to string
+ * @param {number} n number of chars to keep at front/end
+ * @returns {string}
+ */
+export const getEllipsisTxt = (str, n = 6) => {
+  if (str) {
+    return `${str.slice(0, n)}...${str.slice(str.length - n)}`
+  }
+  return ""
 }
 export default NavBar
